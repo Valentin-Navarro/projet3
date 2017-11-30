de29bd43bbc2a16fc2ed15048d8c2cddbb11baf9
sylvestre Marie <sylvestre.marie@gmail.com>
2017-11-30 11:21:56 +0100

    ajout des fonctions

--- Modele/ManagerBillet.php
+++ Modele/ManagerBillet.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
-* 
-*/
 class ManagerBillet
 {
 
@@ -33,8 +30,71 @@ class ManagerBillet
     	return $billets; 
  	}
 
+ 	public function getBillet($idBillet) {
+	    /*$sql = 'select BIL_ID as id, BIL_DATE as date,'
+	      . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+	      . ' where BIL_ID=?';
+	    $billet = $this->executerRequete($sql, array($idBillet));
+	    if ($billet->rowCount() == 1)
+	      return $billet->fetch();  // Accès à la première ligne de résultat
+	    else
+	      throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");*/
+
+	  	$billet = [];
+	  	$requet = $this->db->query("SELECT BIL_ID AS id, BIL_DATE AS dateb, BIL_TITRE AS titre, BIL_CONTENU AS contenu FROM T_BILLET WHERE BIL_ID");
+	  	while ($data = $requete->fetch(PDO::FETCH_ASSOC)){
+    	$billet[] = new Billet($data);
+
+    	if ($billet->rowCount() == 1)
+	      return $billet->fetch();  // Accès à la première ligne de résultat
+	    else
+	      throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+    	}
+   	}
 
 
+//partie administration
+
+   	public function ajouterBillet($titre,$myTextarea) {
+	    /*$sql= 'insert into T_BILLET (BIL_DATE,BIL_TITRE,BIL_CONTENU)values (?,?,?)';
+	    $date = date('Y-m-d H:i:s');  
+	    $this->executerRequete($sql, array($date, $titre, $myTextarea));*/
+
+	    $requet = $this->db->prepare("INSERT INTO T_BILLET(BIL_DATE,BIL_TITRE,BIL_CONTENU) VALUES(:dateb, :titre, :contenu)"); 
+	    $date = date('Y-m-d H:i:s'); 
+
+	    $requet->bindValue(':dateb', $titre->date()); 
+	    $requet->bindValue(':titre', $titre->titre()); 
+	    $requet->bindValue(':contenu', $myTextarea->contenu());
+
+	    $requet->execute();
+
+    }
+
+    public function majBillet($idBillet,$titre,$myTextarea) {
+     	/*$sql ='update T_BILLET set BIL_DATE=?, BIL_TITRE=?, BIL_CONTENU=? where BIL_ID=?';
+     	$date = date('Y-m-d H:i:s'); 
+     	$this->executerRequete($sql, array($date,$titre,$myTextarea,$idBillet));*/
+
+     	$requet = $this->db->prepare("UPDATE T_BILLET SET BIL_DATE = :dateb, BIL_TITRE = :titre ,BIL_CONTENU = :contenu WHERE BIL_ID = :id"); 
+	    $date = date('Y-m-d H:i:s'); 
+
+	    $requet->bindValue(':dateb', $titre->date()); 
+	    $requet->bindValue(':titre', $titre->titre()); 
+	    $requet->bindValue(':contenu', $myTextarea->contenu());
+	    $requet->bindValue(':id', $myTextarea->id());
+	    
+	    $requet->execute();
+	}
+
+	public function suppBillet($idBillet) {
+       /*$sql ='delete from T_BILLET where BIL_ID=?';
+       $this->executerRequete($sql, array($idBillet));
+       $sql ='delete from T_COMMENTAIRE where BIL_ID=?';
+       $this->executerRequete($sql, array($idBillet));*/
+       $requet = $this->db->exec('DELETE FROM T_BILLET WHERE BIL_ID = '.$idBillet->id()); 
+       $requet = $this->db->exec('DELETE FROM T_COMMENTAIRE WHERE BIL_ID = '.$idBillet->id());
+    }
 }
 
 
